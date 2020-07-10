aa8cec4445ec079c04911e08d2c6e8a1597e84af
kento0805-naka <kentz.eva.a.r.xxx@gmail.com>
2020-07-10 19:26:00 +0900

    vuetifyの機能を学習

--- src/App.vue
+++ src/App.vue
@@ -1,149 +1,69 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      app
+      v-model="nav"
+      absolute
+      temporary
+     >
+      
+    </v-navigation-drawer>
+
+    <v-app-bar 
       app
       color="primary"
       dark
+      collapse-on-scroll
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>アドレス帳</v-toolbar-title>
       <v-spacer></v-spacer>
+      
+        <v-avatar color="blue" size="36">K</v-avatar>
+      
+      
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <v-row align="center">
-        <v-row justify="space-around">
-          <v-switch v-model="valid" class="ma-4" label="Valid" readonly></v-switch>
-          <v-switch v-model="lazy" class="ma-4" label="Lazy"></v-switch>
-        </v-row>
-        <v-form
-          ref="form"
-          v-model="valid"
-          :lazy-validation="lazy"
-        >
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Name"
-            required
-          ></v-text-field>
+    <v-main>
 
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+      <v-container fluid>
+        <div>
+          <v-card>
+            <v-responsive
+              :aspect-ratio="16/9"
+            >
+              <v-card-title>メインコンテンツのタイトル</v-card-title>
+              <v-card-text>
+                ここにはコンテンツテキストが入ります。<br>
+                改行は反映されていますか？
+              </v-card-text>
+              <v-btn
+                @click.stop="nav = !nav"
+                color="primary"
+              >ボタン</v-btn>
+            </v-responsive>
+          </v-card>
+        </div>
 
-          <v-select
-            v-model="select"
-            :items="items"
-            :rules="[v => !!v || 'Item is required']"
-            label="Item"
-            required
-          ></v-select>
+        
+      </v-container>
+    </v-main>
 
-          <v-checkbox
-            v-model="checkbox"
-            :rules="[v => !!v || 'You must agree to continue!']"
-            label="Do you agree?"
-            required
-          ></v-checkbox>
-
-          <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-          >
-            Validate
-          </v-btn>
-
-          <v-btn
-            color="error"
-            class="mr-4"
-            @click="reset"
-          >
-            Reset Form
-          </v-btn>
-
-          <v-btn
-            color="warning"
-            @click="resetValidation"
-          >
-            Reset Validation
-          </v-btn>
-        </v-form>
-      </v-row>
-    </v-content>
+    <v-footer app>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
-
 export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-      lazy: false,
-    }),
+  data() {
+    return {
+      nav: null,
+      hover: false,
+    }
+  },
+  
 
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
-    },
-  }
+}
 </script>
