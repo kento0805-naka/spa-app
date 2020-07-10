b0d2bef91a1a8fc6162dacd269744aa376528f3b
kento0805-naka <kentz.eva.a.r.xxx@gmail.com>
2020-07-10 19:32:47 +0900

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,27 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      app
-      v-model="nav"
-      absolute
-      temporary
-     >
-      
-    </v-navigation-drawer>
-
-    <v-app-bar 
-      app
-      color="primary"
-      dark
-      collapse-on-scroll
-    >
+    <v-app-bar app >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>アドレス帳</v-toolbar-title>
+      <v-toolbar-title>
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      
-        <v-avatar color="blue" size="36">K</v-avatar>
-      
-      
-
     </v-app-bar>
 
-    <v-main>
-
-      <v-container fluid>
-        <div>
-          <v-card>
-            <v-responsive
-              :aspect-ratio="16/9"
-            >
-              <v-card-title>メインコンテンツのタイトル</v-card-title>
-              <v-card-text>
-                ここにはコンテンツテキストが入ります。<br>
-                改行は反映されていますか？
-              </v-card-text>
-              <v-btn
-                @click.stop="nav = !nav"
-                color="primary"
-              >ボタン</v-btn>
-            </v-responsive>
-          </v-card>
-        </div>
-
-        
-      </v-container>
-    </v-main>
-
-    <v-footer app>
-    </v-footer>
+    <v-content>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      nav: null,
-      hover: false,
-    }
+  name: 'App',
+  components: {
   },
-  
-
+  data () {
+    return {
+      //
+    }
+  }
 }
 </script>
